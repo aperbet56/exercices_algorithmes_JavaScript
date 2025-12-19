678c81391903b16a3e3899bfa7c67c6cbe5426d6
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-12-19 16:42:10 +0100

    Ajout de la solution à exercice 25

--- exercice25.js
+++ exercice25.js
@@ -1,1 +1,10 @@
 // Exercice 25 : Créer une fonction qui utilise une condition combinée && pour vérifier la longueur password.length d'au moins >= 9 et contenir le caractère "@" avec password.includes.
+
+const checkPassword = (password) => {
+  return password.length >= 9 && password.includes("@");
+};
+
+// Exemples
+console.log(`allez1234 : ${checkPassword("allez1234")}`); // false
+console.log(`orange@sio8 : ${checkPassword("orange@sio8")}`); // true
+console.log(`ol,@69 : ${checkPassword("ol,@69")}`); // false
