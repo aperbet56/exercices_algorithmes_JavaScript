8dfc21c8bbbfb98df438b62f12ab8073ec7ac909
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-12-19 16:38:11 +0100

    Création du fichier exercice25.js

---
+++ exercice25.js
@@ -0,0 +1,1 @@
+// Exercice 25 : Créer une fonction qui utilise une condition combinée && pour vérifier la longueur password.length d'au moins >= 9 et contenir le caractère "@" avec password.includes.
