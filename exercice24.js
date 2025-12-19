045e84d82624c15fd70a3bd8c1d1cc7b2a5c17fd
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-12-19 16:23:54 +0100

    Création du fichier exercice24.js

---
+++ exercice24.js
@@ -0,0 +1,1 @@
+// Créer d'une fonction addPrice qui prendre en paramètres un prix initial et un pourcentage d'augmentation(price, percentage). Cette fonction doit calculer l'augmentation en multipliant le prix par le pourcentage divisé par 100 ( prix X (pourcentage / 100)). Elle doit ajouter ensuite l'augmentation au prix initial prix + augmentation pour obtenir le prix final.
