55e3a0982bb83f784beeff35a3b07cbe40319277
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-12-19 17:04:18 +0100

    Ajout de la solution à l'exercice 26

--- exercice26.js
+++ exercice26.js
@@ -1,1 +1,22 @@
 // Exercice 26 : Calculer la moyenne d'un tableau
+
+const calcAverage = (array) => {
+  // Condition if qui vérifie si le tableau est vide
+  if (array.length === 0) {
+    return 0;
+  }
+
+  // Création de la variable sum
+  let sum = 0;
+
+  // Boucle for qui va parcourir le tableau
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i]; // Ajoute chaque nombre à la somme
+  }
+
+  return sum / array.length; // Retourne la moyenne
+};
+
+// Exemple
+let array = [14, 20, 12, 18, 8];
+console.log(`[${array}] Moyenne : ${calcAverage(array)}`); // 14.4
